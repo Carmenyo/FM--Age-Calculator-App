153b2677c75d1b47e48c629fd1a9d5e89aa7c072
Carmen Castro <carmencastro@MacBook-Air-de-Carmen.local>
2024-02-27 22:31:38 +0100

    const JS

---
+++ main.js
@@ -0,0 +1,9 @@
+// Selecciona los elementos del DOM
+    const input = document.querySelectorAll(".input");
+    const label = document.querySelectorAll(".label small");
+    const displayAge = document.querySelectorAll(".display span");
+    const errorMessage = document.querySelectorAll(".error");
+    const dayValue = document.querySelectorAll(".day");
+    const monthValue = document.querySelectorAll(".month");
+    const yearValue = document.querySelectorAll(".year");
+    const button = document.querySelectorAll(".submit-button");
