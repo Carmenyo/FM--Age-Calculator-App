7c7cc5012b00590390020a14b7af59728fd3fc5b
Carmen Castro <carmencastro@MacBook-Air-de-Carmen.local>
2024-02-28 15:22:39 +0100

    js working

--- main.js
+++ main.js
@@ -1,9 +1,78 @@
 // Selecciona los elementos del DOM
-    const input = document.querySelectorAll(".input");
-    const label = document.querySelectorAll(".label small");
-    const displayAge = document.querySelectorAll(".display span");
-    const errorMessage = document.querySelectorAll(".error");
-    const dayValue = document.querySelectorAll(".day");
-    const monthValue = document.querySelectorAll(".month");
-    const yearValue = document.querySelectorAll(".year");
-    const button = document.querySelectorAll(".submit-button");
+const input = document.querySelectorAll("input");
+const errorMessage = document.querySelectorAll(".error");
+const button = document.querySelector(".submit-button");
+const day = document.querySelector(".display-days");
+const month = document.querySelector(".display-months");
+const year = document.querySelector(".display-years");
+
+// Función para mostrar u ocultar mensaje de error
+const toggleError = (index, show) => {
+    errorMessage[index].style.opacity = show ? 1 : 0;
+}
+
+// Función
+const calculateAge = () => {
+    // Verifica si los campos de entrada están vacíos
+    let isEmpty = false;
+    input.forEach((element, index) => {
+        const isNumeric = !isNaN(element.value);
+        toggleError(index, !element.value || !isNumeric);
+        if (!element.value || !isNumeric) {
+            isEmpty = true;
+        }
+    });
+
+    // Si algún campo está vacío o no es numérico, no realiza el cálculo
+    if (isEmpty) {
+        return;
+    }
+
+    // Obtiene la fecha actual
+    const todayDate = new Date();
+    const todayDay = todayDate.getDate();
+    const todayMonth = todayDate.getMonth() + 1;
+    const todayYear = todayDate.getFullYear();
+
+    // Obtiene los valores de los campos de entrada
+    const inputDay = parseInt(input[0].value);
+    const inputMonth = parseInt(input[1].value);
+    const inputYear = parseInt(input[2].value);
+
+    // Calcula la edad
+    let y = todayYear - inputYear;
+    let m = todayMonth - inputMonth;
+    let d = todayDay - inputDay;
+
+    if (d < 0) {
+        m -= 1;
+        d += numberOfMonth[inputMonth - 1];
+    }
+    if (m < 0) {
+        y -= 1;
+        m += 12;
+    }
+
+    // Actualiza la visualización de la edad
+    day.textContent = d;
+    month.textContent = m;
+    year.textContent = y;
+
+    // Oculta los mensajes de error
+    errorMessage.forEach(error => {
+        error.style.opacity = 0;
+    });
+}
+
+// Evento
+input.forEach((element, index) => {
+    element.addEventListener("input", () => {
+        toggleError(index, !element.value || isNaN(element.value));
+        calculateAge();
+    });
+});
+
+// Oculta los mensajes de error al principio
+errorMessage.forEach(error => {
+    error.style.opacity = 0;
+});
